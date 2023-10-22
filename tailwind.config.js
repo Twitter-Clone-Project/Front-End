36a73fe7d6aadd56591be94b96b441a39e8417c7
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2023-10-22 18:00:44 +0300

    add Twitter colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      blue: "#1DA1F2",
+      black: "#14171A",
+      white: "#FFFFFF",
+      "dark-gray": "#657786",
+      "light-gray": "#AAB8C2",
+      "x-light-gray": "#E1E8ED",
+      "xx-light-gray": "#F5F8FA",
+      warning: "#F4212E"
+    },
   },
   plugins: [],
 }
