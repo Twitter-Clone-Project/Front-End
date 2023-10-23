f77979c1c4f1b13d4f957b0e1a0c39973a700ec1
MohamedMaher02 <mohamed.02maher@gmail.com>
2023-10-23 12:19:01 +0300

    Creating Button Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import * as React from 'react';
-import EmailInput from './form-controls/emailInput';
-import NameInput from './form-controls/nameInput';
-import PasswordInput from './form-controls/passwordInput';
 
 function App() {
   return (
     <div
       data-k="dummy"
-      className="ml-80 mt-80 flex h-80 w-80 flex-col items-center justify-end gap-8 bg-white "
+      className="flex h-screen w-full items-center justify-center bg-sky-500 text-center text-4xl"
     >
-      <NameInput />
-      <EmailInput />
-      <PasswordInput />
+      Hello Twitter!
     </div>
   );
 }
