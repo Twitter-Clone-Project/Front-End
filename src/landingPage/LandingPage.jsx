d3c77317572a29b643652b246afba15c85d99f03
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2023-10-24 19:15:53 +0300

    refactor new account form into new component

--- src/landingPage/LandingPage.jsx
+++ src/landingPage/LandingPage.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import GoogleSignInBtn from '../form-controls/GoogleSignIn';
-import Button from '../form-controls/Button';
+import NewAccountForm from './NewAccountForm';
 
 function LandingPage() {
   return (
@@ -24,34 +23,7 @@ function LandingPage() {
             Happening now
           </span>
           <p className="text-2xl font-extrabold sm:text-3xl">Join today.</p>
-          <div className="logging flex w-[300px] flex-col">
-            <GoogleSignInBtn />
-            <div className="relative flex items-center py-1">
-              <div className="border-border-gray flex-grow border-t" />
-              <p className="px-2 font-thin">or</p>
-              <div className="border-border-gray flex-grow border-t" />
-            </div>
-            <Button
-              backGroundColor="blue"
-              label="Create account"
-              borderColor="none"
-              labelColor="white"
-            />
-            <p className="text-light-thin py-2 text-xs font-extralight">
-              By signing up, you agree to the <a href="/">Terms of Service</a>{' '}
-              and <a href="/">Privacy Policy</a>, including{' '}
-              <a href="/">Cookie Use</a>.
-            </p>
-            <div>
-              <p className="mt-10 py-3">Already have an account?</p>
-              <Button
-                backGroundColor="transparant"
-                label="Sign in"
-                labelColor="blue"
-                borderColor="gray"
-              />
-            </div>
-          </div>
+          <NewAccountForm />
         </div>
         <div className="footer text-light-thin self-end justify-self-center p-4 text-sm lg:col-span-2">
           <p>&copy; Copyrights by SWE Front-End Team</p>
