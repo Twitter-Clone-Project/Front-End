37f1ea10892f31462e52b4c43407c77ecf9881ef
MohamedMaher02 <mohamed.02maher@gmail.com>
2023-11-04 16:58:31 +0200

    Update Button component
    Add customized dark mode for button component

--- src/form-controls/Button.jsx
+++ src/form-controls/Button.jsx
@@ -1,9 +1,27 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react';
 
+<<<<<<< Updated upstream
 function Button({ backGroundColor, borderColor, labelColor, label, path }) {
   return (
     <div className="relative h-[38.5px] w-full">
+=======
+function Button({
+  backGroundColor,
+  backGroundColorDark,
+  borderColor,
+  labelColor,
+  labelColorDark,
+  label,
+  textSize,
+  path,
+  width,
+  hight,
+  children,
+}) {
+  return (
+    <div className={`relative ${hight || 'h-[38.5px]'} ${width || 'w-full'}`}>
+>>>>>>> Stashed changes
       <button
         type="submit"
         className={` absolute rounded-3xl ${
@@ -11,12 +29,37 @@ function Button({ backGroundColor, borderColor, labelColor, label, path }) {
         }  ${
           backGroundColor === 'white' ? 'bg-white hover:bg-[#e6e6e6]' : ''
         }  ${
+<<<<<<< Updated upstream
           backGroundColor === 'black' ? 'bg-black hover:bg-[#031018]' : ''
         } h-full w-full border ${
           borderColor === 'blue' ? 'border-blue' : ''
         }  ${borderColor === 'white' ? 'border-white' : ''}  ${
           borderColor === 'black' ? 'border-black' : ''
         }`}
+=======
+          backGroundColor === 'black' ? 'bg-pure-black hover:bg-[#031018]' : ''
+        }  ${backGroundColor === 'red' ? 'bg-[#f7e1e3] ' : ''} ${
+          backGroundColorDark === 'blue'
+            ? 'dark:bg-blue dark:hover:bg-[#1a8cd8]'
+            : ''
+        }  ${
+          backGroundColorDark === 'white'
+            ? 'dark:bg-white dark:hover:bg-[#e6e6e6]'
+            : ''
+        }  ${
+          backGroundColorDark === 'black'
+            ? 'dark:bg-pure-black dark:hover:bg-[#031018]'
+            : ''
+        }  ${backGroundColorDark === 'red' ? 'dark:bg-[#200a0c] ' : ''}
+        h-full w-full border ${borderColor === 'blue' ? 'border-blue' : ''}  ${
+          borderColor === 'white' ? 'border-white' : ''
+        }  ${borderColor === 'black' ? 'border-black' : ''} ${
+          borderColor === 'gray' ? 'border-light-gray' : ''
+        } ${
+          borderColor === 'red' ? 'border-[#fdc9ce] dark:border-[#67070f]' : ''
+        }
+        ${borderColor === 'none' ? 'border-none' : ''}`}
+>>>>>>> Stashed changes
       />
       <div className="pointer-events-none absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 transform justify-center">
         {path && (
@@ -30,11 +73,16 @@ function Button({ backGroundColor, borderColor, labelColor, label, path }) {
           </svg>
         )}
         <span
-          className={`flex text-base font-bold ${
+          className={`flex ${textSize || 'text-base'}  font-bold ${
             labelColor === 'blue' ? 'text-blue' : ''
           }  ${labelColor === 'white' ? 'text-white ' : ''}  ${
             labelColor === 'black' ? 'text-black' : ''
-          }  items-start`}
+          }  ${labelColor === 'red' ? 'text-warning' : ''}
+            ${labelColorDark === 'blue' ? 'dark:text-blue' : ''}  ${
+              labelColorDark === 'white' ? 'dark:text-white ' : ''
+            }  ${labelColorDark === 'black' ? 'dark:text-black' : ''}
+            ${labelColorDark === 'red' ? 'text-warning' : ''}
+          items-start`}
         >
           {label}
         </span>
