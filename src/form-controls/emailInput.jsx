b2c76f71bf2797d6f98bc579cec2a5ab630232b6
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2023-11-05 19:44:31 +0200

    Change the email state if the input value stays the same for 0.5 seconds

--- src/form-controls/emailInput.jsx
+++ src/form-controls/emailInput.jsx
@@ -1,21 +1,31 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 
 // eslint-disable-next-line no-unused-vars
 function EmailInput({ error, setError, email, setEmail }) {
   const errorMessage = 'Please enter a valid email.';
+  const [value, setValue] = useState('');
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+    clearTimeout(timeoutId);
+    const newTimeouId = setTimeout(() => {
+      if (event.target.checkValidity()) setError('');
+      else setError(errorMessage);
+      setEmail(event.target.value);
+    }, 500);
+    setTimeoutId(newTimeouId);
+  };
+
   return (
     <div className="relative h-[56.44px] w-full items-center justify-center  bg-white dark:bg-black">
       <input
         placeholder=""
         id="arrow"
         type="email"
-        value={email}
-        onChange={(event) => {
-          if (event.target.checkValidity()) setError('');
-          else setError(errorMessage);
-          setEmail(event.target.value);
-        }}
+        value={value}
+        onChange={handleInputChange}
         className={`
         peer h-full w-full rounded px-2 pt-4 text-lg
         outline outline-1 
