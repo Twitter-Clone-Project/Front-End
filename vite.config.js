95b61b193965e80295d3c6b277c7b498b64e1f03
Ahmed ELKady <93336002+goushaa@users.noreply.github.com>
2023-11-09 00:30:54 +0200

    update vite.config.js to not only work on localhost

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', 
+    port: 5173, 
+  },
   test: {
     globals: true,
     environment: 'jsdom',
