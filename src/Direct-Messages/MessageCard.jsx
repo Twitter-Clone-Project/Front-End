c299497b3493140f84608a53ce76b4ebd999652c
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2023-11-23 21:22:43 +0200

    Implemented the message card and message

---
+++ src/Direct-Messages/MessageCard.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MessageCard({ Message, id, clicked, lastMessageId, state }) {
+  return (
+    <div
+      className={`flex w-full flex-col  dark:bg-black ${
+        state === 'send' ? 'items-end ' : 'items-start'
+      } `}
+    >
+      <div
+        className={`
+        flex h-11 w-fit 
+        px-4 py-3 text-base dark:text-white
+      
+        ${
+          id === lastMessageId
+            ? 'rounded-bl-3xl  rounded-br-[4px] rounded-tl-3xl rounded-tr-3xl'
+            : 'rounded-3xl'
+        } 
+        ${
+          state === 'send'
+            ? 'bg-blue hover:bg-[#1a8cd8]'
+            : 'bg-[#EFF3F4] dark:bg-[#2F3336] '
+        } 
+
+       `}
+      >
+        {Message}
+      </div>
+      <div className="h-2">
+        {id === clicked && (
+          <div className=" w-fit cursor-pointer text-sm text-[#536571] hover:underline">
+            {state}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+MessageCard.propTypes = {
+  Message: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  clicked: PropTypes.number.isRequired,
+  lastMessageId: PropTypes.number.isRequired,
+  state: PropTypes.string.isRequired,
+};
+
+export default MessageCard;
