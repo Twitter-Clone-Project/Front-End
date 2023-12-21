fd4d445dc4a7344f862bd65e30759099b45654e4
Mostafa Elsayed <mostafa.elsayed.2002@gmail.com>
2023-12-21 02:21:29 +0200

    Enable images overlap within the person card

--- src/components/Direct-Messages/PersonCard.jsx
+++ src/components/Direct-Messages/PersonCard.jsx
@@ -6,6 +6,7 @@ import { ChatContext } from '../../hooks/ContactContext';
 
 function PersonCard({ imgRef }) {
   const { chatContext } = useContext(ChatContext);
+  console.log(chatContext.contact.commonFollowers.length);
 
   return (
     <div className="mb-4 flex h-[271.555px] w-full flex-col  items-center border-b-[1px] border-[#f6f8f9] px-4 py-5 hover:bg-[#f0f3f3] dark:border-[#252829] dark:bg-black dark:hover:bg-[#16171a]">
@@ -42,28 +43,42 @@ function PersonCard({ imgRef }) {
         data-testid="person-card-common-followers"
         className="flex flex-row"
       >
-        {chatContext.contact.commonFollowers.map((follower, index) => (
-          <div
-            data-testid={`${index}-follower-image`}
-            key={index}
-          >
-            <img
-              className="h-4 w-4 rounded-full"
-              src={follower.imageUrl}
-              alt=""
-            />
-          </div>
-        ))}
-        <div className="ml-3 text-center text-xs text-[#71767B]">
-          Followed by
+        <div className="relative">
+          {chatContext.contact.commonFollowers.map((follower, index) => (
+            <div
+              data-testid={`${index}-follower-image`}
+              key={index}
+            >
+              <img
+                className={`h-4 w-4 rounded-full border
+                 border-white dark:border-black ${
+                   index !== 0 ? '' : 'absolute right-2'
+                 }`}
+                src={follower.imageUrl}
+                alt=""
+              />
+            </div>
+          ))}
         </div>
+
+        {chatContext.contact.commonFollowers.length > 0 && (
+          <div
+            data-testid="followed-by"
+            className="ml-2 text-center text-xs text-[#71767B]"
+          >
+            Followed by
+          </div>
+        )}
         {chatContext.contact.commonFollowers.map((follower, index) => (
           <div
             data-testid={`${index}-follower-name`}
             key={index}
-            className="ml-3 text-center text-xs text-[#71767B]"
+            className="ml-1 text-center text-xs text-[#71767B]"
           >
             {follower.name}
+            {chatContext.contact.commonFollowers.length - 1 !== index
+              ? ','
+              : ''}
           </div>
         ))}
       </div>
