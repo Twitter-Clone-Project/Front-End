25701096774a70a05c3ff7d70ce4baf92a7ad81b
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2023-12-22 10:02:08 +0200

    fixing tabs authentication

--- src/contexts/Auth/reducer.js
+++ src/contexts/Auth/reducer.js
@@ -1,12 +1,18 @@
 export default function reducer(state, action) {
   switch (action.type) {
     case 'LOGIN':
+      localStorage.setItem('user', action.payload.username);
+      localStorage.setItem('isAuthenticated', true);
       return {
         ...state,
         isAuthenticated: true,
         user: action.payload,
       };
     case 'LOGOUT':
+      if (state.isAuthenticated) {
+        localStorage.setItem('user', null);
+        localStorage.setItem('isAuthenticated', 'false');
+      }
       return {
         ...state,
         isAuthenticated: false,
