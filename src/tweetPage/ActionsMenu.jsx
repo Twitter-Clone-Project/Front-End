01eba7e41476d0d694936750636382cd1a5f2219
fatmaebrahim <fatmaebrahim3131@gmail.com>
2023-12-23 16:19:08 +0200

    follow-unfollow

--- src/tweetPage/ActionsMenu.jsx
+++ src/tweetPage/ActionsMenu.jsx
@@ -89,7 +89,7 @@ function ActionsMenu({ userId, tweet, tweets, setTweets }) {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         } else {
-          toggleFollowed(!tweet.user.isFollowed);
+          toggleFollowed((prev) => !prev);
         }
         return response.json();
       })
@@ -122,7 +122,7 @@ function ActionsMenu({ userId, tweet, tweets, setTweets }) {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         } else {
-          toggleFollowed(!tweet.user.isFollowed);
+          toggleFollowed((prev) => !prev);
         }
         return response.json();
       })
@@ -243,13 +243,13 @@ function ActionsMenu({ userId, tweet, tweets, setTweets }) {
                       viewBox="0 0 25 25"
                       className="h-[20px] w-[20px]  "
                     >
-                      {!tweet.user.isFollowed && (
+                      {!followed && (
                         <path
                           className="dark:fill-white"
                           d="M10 4c-1.105 0-2 .9-2 2s.895 2 2 2 2-.9 2-2-.895-2-2-2zM6 6c0-2.21 1.791-4 4-4s4 1.79 4 4-1.791 4-4 4-4-1.79-4-4zm13 4v3h2v-3h3V8h-3V5h-2v3h-3v2h3zM3.651 19h12.698c-.337-1.8-1.023-3.21-1.945-4.19C13.318 13.65 11.838 13 10 13s-3.317.65-4.404 1.81c-.922.98-1.608 2.39-1.945 4.19zm.486-5.56C5.627 11.85 7.648 11 10 11s4.373.85 5.863 2.44c1.477 1.58 2.366 3.8 2.632 6.46l.11 1.1H1.395l.11-1.1c.266-2.66 1.155-4.88 2.632-6.46z"
                         />
                       )}
-                      {tweet.user.isFollowed && (
+                      {followed && (
                         <path
                           className="dark:fill-white"
                           d="M10 4c-1.105 0-2 .9-2 2s.895 2 2 2 2-.9 2-2-.895-2-2-2zM6 6c0-2.21 1.791-4 4-4s4 1.79 4 4-1.791 4-4 4-4-1.79-4-4zm12.586 3l-2.043-2.04 1.414-1.42L20 7.59l2.043-2.05 1.414 1.42L21.414 9l2.043 2.04-1.414 1.42L20 10.41l-2.043 2.05-1.414-1.42L18.586 9zM3.651 19h12.698c-.337-1.8-1.023-3.21-1.945-4.19C13.318 13.65 11.838 13 10 13s-3.317.65-4.404 1.81c-.922.98-1.608 2.39-1.945 4.19zm.486-5.56C5.627 11.85 7.648 11 10 11s4.373.85 5.863 2.44c1.477 1.58 2.366 3.8 2.632 6.46l.11 1.1H1.395l.11-1.1c.266-2.66 1.155-4.88 2.632-6.46z"
@@ -257,12 +257,12 @@ function ActionsMenu({ userId, tweet, tweets, setTweets }) {
                       )}
                     </svg>
                   </div>
-                  {!tweet.user.isFollowed && (
+                  {!followed && (
                     <div className="  px-[14px] py-[16px] text-[15px] font-semibold">
                       Follow @{tweet.user.username}
                     </div>
                   )}
-                  {tweet.user.isFollowed && (
+                  {followed && (
                     <div className="  px-[14px] py-[16px] text-[15px] font-semibold">
                       Unfollow @{tweet.user.username}
                     </div>
