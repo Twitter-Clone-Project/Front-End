9fd2bb81f2040c9bfac5dbe6c9631501462aa6bc
nouraymanh <102790603+nouraymanh@users.noreply.github.com>
2023-12-23 23:38:08 +0200

    Updates

--- src/components/search-bar/ResultsList.jsx
+++ src/components/search-bar/ResultsList.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
